71f69ecf86e939d609304ced4d4b1e3d14d84002
Dmytro Stebaiev <dmytro@skalelabs.com>
2022-11-04 17:28:13 +0200

    Refactor Marionette

--- contracts/Marionette.sol
+++ contracts/Marionette.sol
@@ -32,12 +32,6 @@ contract Marionette is IMarionette, AccessControlEnumerableUpgradeable {
     using AddressUpgradeable for address;
     using AddressUpgradeable for address payable;
 
-    struct FunctionCall {
-        address receiver;
-        uint value;
-        bytes data;
-    }
-
     bytes32 public constant IMA_ROLE = keccak256("IMA_ROLE");
     bytes32 public constant PUPPETEER_ROLE = keccak256("PUPPETEER_ROLE");
     string public constant ACCESS_VIOLATION = "Access violation";
@@ -90,10 +84,16 @@ contract Marionette is IMarionette, AccessControlEnumerableUpgradeable {
         require(hasRole(IMA_ROLE, msg.sender), "Sender is not IMA");
         require(hasRole(PUPPETEER_ROLE, sender), ACCESS_VIOLATION);
 
-        FunctionCall memory functionCall = _parseFunctionCall(data);
+        FunctionCall[] memory functionCalls = _parseFunctionCalls(data);
 
-        bytes memory output = _doCall(payable(functionCall.receiver), functionCall.value, functionCall.data);        
-        emit FunctionCallResult(output);
+        for (uint i = 0; i < functionCalls.length; ++i) {
+            bytes memory output = _doCall(
+                payable(functionCalls[i].receiver),
+                functionCalls[i].value,
+                functionCalls[i].data
+            );
+            emit FunctionCallResult(output);
+        }
     }
 
     function execute(
@@ -108,17 +108,39 @@ contract Marionette is IMarionette, AccessControlEnumerableUpgradeable {
     {
         require(hasRole(PUPPETEER_ROLE, msg.sender), ACCESS_VIOLATION);
 
+        if (msg.value > 0) {
+            emit EtherReceived(msg.sender, msg.value);
+        }
+
         return _doCall(target, value, data);
+    }
+
+    function executeMultiple(FunctionCall[] calldata functionCalls) external payable override returns (bytes[] memory) {
+        require(hasRole(PUPPETEER_ROLE, msg.sender), ACCESS_VIOLATION);
+
+        if (msg.value > 0) {
+            emit EtherReceived(msg.sender, msg.value);
+        }
+
+        bytes[] memory results = new bytes[](functionCalls.length);
+        for (uint i = 0; i < functionCalls.length; ++i) {
+            results[i] = _doCall(payable(functionCalls[i].receiver), functionCalls[i].value, functionCalls[i].data);
+        }
+        return results;
     }
 
     function sendSFuel(address payable target, uint value) external payable override {
         require(hasRole(PUPPETEER_ROLE, msg.sender), ACCESS_VIOLATION);
 
+        if (msg.value > 0) {
+            emit EtherReceived(msg.sender, msg.value);
+        }
+
         _doCall(target, value, "0x");
     }
 
     function setVersion(string calldata newVersion) external override {
-        if (!hasRole(DEFAULT_ADMIN_ROLE, msg.sender)) 
+        if (!hasRole(DEFAULT_ADMIN_ROLE, msg.sender))
             revert Unauthorized(msg.sender);
         emit VersionUpdated(version, newVersion);
         version = newVersion;
@@ -134,54 +156,43 @@ contract Marionette is IMarionette, AccessControlEnumerableUpgradeable {
         override
         returns (bytes memory)
     {
-        return abi.encode(receiver, value, data);
+        FunctionCall[] memory functionCalls = new FunctionCall[](1);
+        functionCalls[0] = FunctionCall({receiver: receiver, value: value, data: data});
+        return encodeFunctionCalls(FunctionCall[](functionCalls));
+    }
+
+    function encodeFunctionCalls(
+        FunctionCall[] memory functionCalls
+    )
+        public
+        pure
+        override
+        returns (bytes memory)
+    {
+        return abi.encode(functionCalls);
     }
 
     // private
 
     function _doCall(address payable target, uint value, bytes memory data) private returns (bytes memory) {
-        if (msg.value > 0) {
-            emit EtherReceived(msg.sender, msg.value);
-        }
-
         if (value > 0) {
             emit EtherSent(target, value);
         }
 
         if (target.isContract()) {
             if (data.length >= 4) {
-                if (_getHashedSelector(data) == _multiSendHashed()) {
-                    // slither-disable-next-line controlled-delegatecall,low-level-calls
-                    (bool success, bytes memory result) = target.delegatecall(data);
-                    // solhint-disable-previous-line avoid-low-level-calls
-                    require(success, "MultiSend failed");
-                    return result;
-                } else {
-                    return target.functionCallWithValue(data, value);
-                }
+                return target.functionCallWithValue(data, value);
             } else {
                 target.sendValue(value);
                 return "0x";
-            }                        
+            }
         } else {
             target.sendValue(value);
             return "0x";
         }
     }
 
-    function _getHashedSelector(bytes memory data) private pure returns (bytes32) {
-        bytes memory selector = new bytes(4);
-        for (uint i = 0; i < 4; i++) {
-            selector[i] = data[i]; 
-        }
-        return keccak256(abi.encodePacked(selector));
-    }
-
-    function _multiSendHashed() private pure returns (bytes32) {
-        return keccak256(abi.encodePacked(bytes4(keccak256(bytes("multiSend(bytes)")))));
-    }
-
-    function _parseFunctionCall(bytes calldata data) private pure returns (FunctionCall memory functionCall) {
-        (functionCall.receiver, functionCall.value, functionCall.data) = abi.decode(data, (address, uint, bytes));
+    function _parseFunctionCalls(bytes calldata data) private pure returns (FunctionCall[] memory functionCalls) {
+        return abi.decode(data, (FunctionCall[]));
     }
 }
