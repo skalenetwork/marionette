8107f80e44d4c2505e450398c94e4c52e66d827e
Dmytro Stebaiev <dmytro@skalelabs.com>
2022-12-30 18:37:36 +0200

    Add upgrade script

--- migrations/upgrade.ts
+++ migrations/upgrade.ts
@@ -1,178 +1,78 @@
 import { ethers } from "hardhat";
 import chalk from "chalk";
-import hre from "hardhat";
-import { SkaleABIFile, getContractKeyInAbiFile, encodeTransaction, concatTransactions, upgrade } from "@skalenetwork/upgrade-tools";
-import { getManifestAdmin } from "@openzeppelin/hardhat-upgrades/dist/admin";
-import { ProxyAdmin } from "@skalenetwork/upgrade-tools/dist/typechain-types";
+import { SafeImaLegacyMarionetteSubmitter, Upgrader } from "@skalenetwork/upgrade-tools";
+import { SkaleABIFile } from "@skalenetwork/upgrade-tools/dist/src/types/SkaleABIFile";
+import { promises as fs } from 'fs';
 
-async function getMarionette(abi: SkaleABIFile) {
-    return ((await ethers.getContractFactory("Marionette")).attach(
-        abi[getContractKeyInAbiFile("Marionette") + "_address"] as string
-    ));
+const marionette_address = "0xD2c0DeFACe000000000000000000000000000000";
+
+async function getMarionette() {
+    return await ethers.getContractAt("Marionette", marionette_address);
 }
 
-export async function getDeployedVersion(abi: SkaleABIFile) {
-    const marionette = await getMarionette(abi);
-    try {
-        return await marionette.version();
-    } catch {
-        console.log(chalk.red("Can't read deployed version"));
+class MarionetteUpgrader extends Upgrader {
+
+    getDeployedVersion = async () => {
+        return await (await getMarionette()).version();
+    };
+
+    setVersion = async (newVersion: string) => {
+        const marionette = await getMarionette();
+        this.transactions.push({
+            to: marionette.address,
+            data: marionette.interface.encodeFunctionData("setVersion", [newVersion])
+        });
     }
 }
 
-export async function setNewVersion(safeTransactions: string[], abi: SkaleABIFile, newVersion: string) {
-    const marionette = await getMarionette(abi);
-    safeTransactions.push(encodeTransaction(
-        0,
-        marionette.address,
-        0,
-        marionette.interface.encodeFunctionData(
-            "execute", 
-            [
-                marionette.address,
-                0,
-                marionette.interface.encodeFunctionData("setVersion", [newVersion])            ]
-        )
-    ));
-}
-
 async function main() {
-    await upgrade(
-        "marionette",
-        "1.0.0",
-        getDeployedVersion,
-        setNewVersion,
-        [],
-        ["Marionette"],
-        // async (safeTransactions, abi, contractManager) => {
-        async () => {
-            // deploy new contracts
-        },
-        async (safeTransactions, abi, _, safeMockAddress) => {
-            const schainName = process.env.SKALE_CHAIN_NAME;
-            const messageProxyForMainnetAddress = process.env.MESSAGE_PROXY_FOR_MAINNET_ADDRESS;
-            const schainHash = ethers.utils.solidityKeccak256(["string"], [schainName]);
-            const proxyAdmin = await getManifestAdmin(hre) as ProxyAdmin;
-            const marionette = await getMarionette(abi);
-            const multiSendFactory = await ethers.getContractFactory("MultiSend");
-            const multiSend = await multiSendFactory.deploy(marionette.address);
-            const imaMockFactory = await ethers.getContractFactory("ImaMock");
-            await multiSend.deployTransaction.wait();
-
-            if (!schainName) {
-                console.log(chalk.red("Set SKALE chain name to environment"));
-                process.exit(1);
-            }
-            if (!messageProxyForMainnetAddress) {
-                console.log(chalk.red("Set address of MessageProxyForMainnet to environment"));
-                process.exit(1);
-            }
-
-            const _safeTransactions: string[] = [];
-
-            let encodedData = imaMockFactory.interface.encodeFunctionData(
-                "postOutgoingMessage",
-                [
-                    schainHash,
-                    marionette.address,
-                    ethers.utils.defaultAbiCoder.encode(["address", "uint", "bytes"], [
-                        marionette.address,
-                        0,
-                        marionette.interface.encodeFunctionData("grantRole", [await marionette.PUPPETEER_ROLE(), multiSend.address])
-                    ])
-                ]
-            );
-            // 1. grant PUPPETEER_ROLE to MultiSend (for execute permissions)
-            _safeTransactions.push(encodeTransaction(
-                0,
-                messageProxyForMainnetAddress,
-                0,
-                encodedData
-            ));
-
-            safeTransactions.unshift(encodeTransaction(
-                0,
-                marionette.address,
-                0,
-                marionette.interface.encodeFunctionData(
-                    "execute", 
-                    [
-                        proxyAdmin.address,
-                        0,
-                        proxyAdmin.interface.encodeFunctionData("transferOwnership", [multiSend.address])
-                    ]
-                )
-            ));
-
-            safeTransactions.push(encodeTransaction(
-                0,
-                marionette.address,
-                0,
-                marionette.interface.encodeFunctionData(
-                    "execute", 
-                    [
-                        marionette.address,
-                        0,
-                        marionette.interface.encodeFunctionData("revokeRole", [await marionette.PUPPETEER_ROLE(), multiSend.address])
-                    ]
-                )
-            ));
-
-            if (safeMockAddress !== undefined) {
-                safeTransactions.push(encodeTransaction(
-                    0,
-                    proxyAdmin.address,
-                    0,
-                    proxyAdmin.interface.encodeFunctionData("transferOwnership", [safeMockAddress])
-                ));
-            } else {
-                safeTransactions.push(encodeTransaction(
-                    0,
-                    proxyAdmin.address,
-                    0,
-                    proxyAdmin.interface.encodeFunctionData("transferOwnership", [marionette.address])
-                ));
-            }
- 
-            encodedData = imaMockFactory.interface.encodeFunctionData(
-                "postOutgoingMessage",
-                [
-                    schainHash,
-                    marionette.address,
-                    ethers.utils.defaultAbiCoder.encode(["address", "uint", "bytes"], [
-                        multiSend.address,
-                        0,
-                        multiSendFactory.interface.encodeFunctionData("multiSend", [ concatTransactions(safeTransactions) ])
-                    ])
-                ]
-            );
-
-            // 2.1. transferOwnership from Marionette to MultiSend (for upgrade permissions)
-            // 2.2. upgrade Marionette
-            // 2.3. setVersion
-            // 2.4. revoke PUPPETEER_ROLE from MultiSend
-            // 2.5. transferOwnership from MultiSend to Marionette
-            // or
-            // 2.6. transferOwnership from Marionette to SafeMock
-            _safeTransactions.push(encodeTransaction(
-                0,
-                messageProxyForMainnetAddress,
-                0,
-                encodedData
-            ));
-            safeTransactions.length = 0;
-            Object.assign(safeTransactions, _safeTransactions);
-        },
-        async (safeMock, abi) => {
-            const proxyAdmin = await getManifestAdmin(hre) as ProxyAdmin;
-            const marionette = await getMarionette(abi);
-            const PUPPETEER_ROLE = await marionette.PUPPETEER_ROLE();
-            const encodedGrantRole = marionette.interface.encodeFunctionData("grantRole", [PUPPETEER_ROLE, safeMock.address]);
-            await marionette.execute(marionette.address, 0, encodedGrantRole);
-
-            await safeMock.transferProxyAdminOwnership(proxyAdmin.address, marionette.address);
+    if (!process.env.IMA_ABI) {
+        console.log(chalk.red("Set path to ima abi to IMA_ABI environment variable"));
+        process.exit(1);
+    }
+    if (!process.env.SAFE_ADDRESS) {
+        console.log(chalk.red("Set Gnosis Safe owner address to SAFE_ADDRESS environment variable"));
+        process.exit(1);
+    }
+    let schainHash;
+    if (!process.env.SCHAIN_HASH) {
+        if (!process.env.SCHAIN_NAME) {
+            console.log(chalk.red("Set schain name to SCHAIN_NAME environment variable"));
+            console.log(chalk.red("or schain hash to SCHAIN_HASH environment variable"));
+            process.exit(1);
+        } else {
+            schainHash = ethers.utils.solidityKeccak256(["string"], [process.env.SCHAIN_NAME]);
         }
+    } else {
+        schainHash = process.env.SCHAIN_HASH;
+    }
+    let mainnetChainId: number;
+    if (!process.env.MAINNET_CHAIN_ID) {
+        console.log(chalk.red("Set chainId of mainnet to MAINNET_CHAIN_ID environment variable"));
+        console.log(chalk.red("Use 1 for Ethereum mainnet or 5 for Goerli"));
+        process.exit(1);
+    } else {
+        mainnetChainId = Number.parseInt(process.env.MAINNET_CHAIN_ID);
+    }
+
+    const imaAbi = JSON.parse(await fs.readFile(process.env.IMA_ABI, "utf-8")) as SkaleABIFile;
+
+    const upgrader = new MarionetteUpgrader(
+        "Marionette",
+        "1.0.0",
+        {
+            "marionette_address": marionette_address
+        },
+        ["Marionette"],
+        new SafeImaLegacyMarionetteSubmitter(
+            process.env.SAFE_ADDRESS,
+            imaAbi,
+            schainHash,
+            mainnetChainId
+        )
     );
+
+    await upgrader.upgrade();
 }
 
 if (require.main === module) {
