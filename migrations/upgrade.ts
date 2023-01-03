f2ac14f2806de1606b0cd5435966d2c40f03e178
Dmytro Stebaiev <dmytro@skalelabs.com>
2023-01-03 17:47:04 +0200

    Update upgrade-tools

--- migrations/upgrade.ts
+++ migrations/upgrade.ts
@@ -1,8 +1,5 @@
 import { ethers } from "hardhat";
-import chalk from "chalk";
-import { SafeImaLegacyMarionetteSubmitter, Upgrader } from "@skalenetwork/upgrade-tools";
-import { SkaleABIFile } from "@skalenetwork/upgrade-tools/dist/src/types/SkaleABIFile";
-import { promises as fs } from 'fs';
+import { Upgrader } from "@skalenetwork/upgrade-tools";
 
 const marionette_address = "0xD2c0DeFACe000000000000000000000000000000";
 
@@ -26,36 +23,6 @@ class MarionetteUpgrader extends Upgrader {
 }
 
 async function main() {
-    if (!process.env.IMA_ABI) {
-        console.log(chalk.red("Set path to ima abi to IMA_ABI environment variable"));
-        process.exit(1);
-    }
-    if (!process.env.SAFE_ADDRESS) {
-        console.log(chalk.red("Set Gnosis Safe owner address to SAFE_ADDRESS environment variable"));
-        process.exit(1);
-    }
-    let schainHash;
-    if (!process.env.SCHAIN_HASH) {
-        if (!process.env.SCHAIN_NAME) {
-            console.log(chalk.red("Set schain name to SCHAIN_NAME environment variable"));
-            console.log(chalk.red("or schain hash to SCHAIN_HASH environment variable"));
-            process.exit(1);
-        } else {
-            schainHash = ethers.utils.solidityKeccak256(["string"], [process.env.SCHAIN_NAME]);
-        }
-    } else {
-        schainHash = process.env.SCHAIN_HASH;
-    }
-    let mainnetChainId: number;
-    if (!process.env.MAINNET_CHAIN_ID) {
-        console.log(chalk.red("Set chainId of mainnet to MAINNET_CHAIN_ID environment variable"));
-        console.log(chalk.red("Use 1 for Ethereum mainnet or 5 for Goerli"));
-        process.exit(1);
-    } else {
-        mainnetChainId = Number.parseInt(process.env.MAINNET_CHAIN_ID);
-    }
-
-    const imaAbi = JSON.parse(await fs.readFile(process.env.IMA_ABI, "utf-8")) as SkaleABIFile;
 
     const upgrader = new MarionetteUpgrader(
         "Marionette",
@@ -63,13 +30,7 @@ async function main() {
         {
             "marionette_address": marionette_address
         },
-        ["Marionette"],
-        new SafeImaLegacyMarionetteSubmitter(
-            process.env.SAFE_ADDRESS,
-            imaAbi,
-            schainHash,
-            mainnetChainId
-        )
+        ["Marionette"]
     );
 
     await upgrader.upgrade();
